f18719c84c01c78cbcb562102ba50e24823d12e5
Sunmon <blackmilk274@gmail.com>
2020-06-11 23:33:18 +0900

    드래그해도 이미지 제대로 표시되도록 수정

--- lib/cards.dart
+++ lib/cards.dart
@@ -1,28 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/physics.dart';
+
+//main() {
+//  runApp(MaterialApp(home: PhysicsCardDragDemo()));
+//}
+
+//class PhysicsCardDragDemo extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(),
+//      body: DraggableCard(
+//        child: FlutterLogo(
+//          size: 128,
+//        ),
+//      ),
+//    );
+//  }
+//}
+
 /// A draggable card that moves back to [Alignment.center] when it's
 /// released.
 class DraggableCard extends StatefulWidget {
   final Widget child;
   DraggableCard({this.child});
+
   @override
   _DraggableCardState createState() => _DraggableCardState();
 }
 
 class _DraggableCardState extends State<DraggableCard>
-with SingleTickerProviderStateMixin{
-  Alignment _dragAlignment = Alignment.center;
+    with SingleTickerProviderStateMixin {
   AnimationController _controller;
+
+  /// The alignment of the card as it is dragged or being animated.
+  ///
+  /// While the card is being dragged, this value is set to the values computed
+  /// in the GestureDetector onPanUpdate callback. If the animation is running,
+  /// this value is set to the value of the [_animation].
+  Alignment _dragAlignment = Alignment.center;
+
   Animation<Alignment> _animation;
 
-  void _runAnimation(Offset pixelsPerSecond, Size size){
+  /// Calculates and runs a [SpringSimulation].
+  void _runAnimation(Offset pixelsPerSecond, Size size) {
     _animation = _controller.drive(
       AlignmentTween(
         begin: _dragAlignment,
         end: Alignment.center,
       ),
     );
-
     // Calculate the velocity relative to the unit interval, [0,1],
     // used by the animation controller.
     final unitsPerSecondX = pixelsPerSecond.dx / size.width;
@@ -39,16 +66,15 @@ with SingleTickerProviderStateMixin{
     final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
 
     _controller.animateWith(simulation);
-
-//    _controller.reset();
-//    _controller.forward();
   }
+
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _controller = AnimationController(vsync: this);
+
     _controller.addListener(() {
-      setState((){
+      setState(() {
         _dragAlignment = _animation.value;
       });
     });
@@ -62,37 +88,30 @@ with SingleTickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return GestureDetector(
-      onPanDown: (details){
+      onPanDown: (details) {
         _controller.stop();
       },
-
-      onPanUpdate: (details){
-        setState((){
-//          _dragAlignment = Alignment(details.delta.dx, details.delta.dy);
+      onPanUpdate: (details) {
+        setState(() {
           _dragAlignment += Alignment(
-//            details.delta.dx,
-//            details.delta.dy
-            details.delta.dx / (size.width /2),
-            details.delta.dy / (size.height /2),
+            details.delta.dx / (size.width / 2),
+//0
+            details.delta.dy / (size.height / 2),
+//            details.delta.dx /2,
+//            details.delta.dy /2,
           );
         });
       },
-      onPanEnd: (details){
+      onPanEnd: (details) {
         _runAnimation(details.velocity.pixelsPerSecond, size);
-//        _runAnimation();
       },
       child: Align(
         alignment: _dragAlignment,
         child: Card(
           child: widget.child,
         ),
-      ),
-    );
-    return Align(
-      child: Card(
-        child: widget.child,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -121,26 +121,37 @@ class HomePageState extends State<HomePage> {
 //      );
 //    }
 
+//    cardList.add(
+//      Align(
+//        alignment: Alignment.topCenter,
+//        child: DraggableCard(
+//          child: FlutterLogo(
+//            size: 250,
+//          )
+//        ),
+//      ),
+//
+//    );
 
+    ///NOTE: FROM HERE
     //FIXME: draggablecard 넣으니까
     // 움직임이 stack 안에서 고정된다....
     // 움직일때 양도 적어지고, stack 밖으로 못 나감!
-
+    /*Alignment _alignment = Alignment.topCenter;
     for(int x = 0; x<CARDS_NUM; x++){
+      _alignment += Alignment(0, 100.0);
       cardList.add(
-        Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-              padding: EdgeInsets.only(top: ((x+20)*10).toDouble()),
-//              padding: EdgeInsets.only(top: planetCard[i].topMargin),
-              child: DraggableCard(
+      Positioned(
+                  child: DraggableCard(
                   child: Card(
                     elevation: 8.0,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20.0),
                     ),
-                   color: Color.fromARGB(250, 112, 19, 179),
+//                    color: Color.fromARGB(250, 112, 19, 179),
+                    color: Colors.green,
                     child: Column(
+//                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Container(
                           decoration: BoxDecoration(
@@ -151,11 +162,10 @@ class HomePageState extends State<HomePage> {
                                 image: AssetImage(
                                     planetCard[x].cardImage
                                 ),
-//                                NetworkImage(),
                                 fit: BoxFit.cover
                             ),
                           ),
-                          height: 300.0,
+                          height: 150.0,
                           width: 320.0,
                         ),
                         Container(
@@ -164,138 +174,63 @@ class HomePageState extends State<HomePage> {
                           child: Column(
                             children: <Widget> [
                               Text(planetCard[x].cardTitle,
-                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.amber),
+                                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.amber),
                               ),
                               Text(planetCard[x].cardText,
                                 style: TextStyle(fontSize: 20, color: Colors.white),
                               ),
-                          ],
+                            ],
                           ),
                         ),
                       ],
                     )),
-              )
-                  ),
-                ),
+              ),
+//             ),
+
+          ),
+//          child: Padding(
+//              padding: EdgeInsets.only(top: ((x+20)*10).toDouble()),
+//              padding: EdgeInsets.only(top: planetCard[i].topMargin),
 //                  child: FlutterLogo(size:128))
       );
-    }
-
+    }*/
+///NOTE: TO HERE
 //    cardList.add(DraggableCard(child: FlutterLogo(size:128)));
 //    cardList.add(DraggableCard(child: FlutterLogo(size:128)));
 //    cardList.add(DraggableCard(child: FlutterLogo(size:128)));
 //    cardList.add(DraggableCard(child: FlutterLogo(size:128)));
-    return cardList;
 
     //card 4개 보여주기
     for (int x = 0; x < CARDS_NUM; x++) {
+      PlanetCard _current = planetCard[x];
       cardList.add(
         Positioned(
           top: planetCard[x].topMargin,
           //TODO: DraggableCard로 바꿔? child Card인데..
-          child: DraggableCard(
-            //TODO: update child by using card items
-            child: FlutterLogo(
-              size: 128,
-            ),
-          ),
-//
-//          child: Draggable(
-//              onDragEnd: (drag) {
-//                removeCards(x);
-//              },
-//
-//              childWhenDragging: Container(),
-//              feedback: GestureDetector( //카드 드래그중일때 설정
-//                onTap: () {
+          child: Draggable(
+              onDragEnd: (drag) {
+//                removeCards(planetCard[x]);
+                removeCards(x);
+              },
+
+              childWhenDragging: Container(),
+              feedback: GestureDetector( //카드 드래그중일때 설정
+                onTap: () {
 //                  print("Hello All");
-//                },
-//                /// 드래그하면 보여지는 카드
-//                child: Card(
-//                  elevation: 8.0,
-//                  shape: RoundedRectangleBorder(
-//                    borderRadius: BorderRadius.circular(20.0),
-//                  ),
-//                   color: Color.fromARGB(250, 112, 19, 179),
-//                  child: Column(
-//                    children: <Widget>[
-//                      Hero(
-//                        tag: "imageTag",
-//                        child: Image.asset(
-//                          planetCard[x].cardImage,
-//                          width: 20.0,
-//                          height: 440.0,
-//                          fit: BoxFit.fill,
-//                        ),
-//                      ),
-//                      Container(
-//                        //드래그할때 보이는 글자 설정
-//                        padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-//                        child: Text(
-//                          planetCard[x].cardTitle,
-//                          style: TextStyle(
-//                            fontSize: 20.0,
-//                            color: Colors.green,
-//                          ),
-//                        ),
-//                      )
-//                    ],
-//                  ),
-//                ),
-//              ),
-//              child: GestureDetector(
-////                onTap: () {
-////                  Navigator.of(context)
-////                      .push(MaterialPageRoute(builder: (BuildContext context) {
-////                    return CardDetails(planetCard[x].cardImage, x);
-////                  }));
-////                },
-//              onTap:(){
-//                //nothing to do
-//              },
-//                /// 기본적으로 화면에 띄워지는 카드
-//                child: Card(
-//                    elevation: 8.0,
-//                    shape: RoundedRectangleBorder(
-//                      borderRadius: BorderRadius.circular(20.0),
-//                    ),
-//                     color: Color.fromARGB(250, 112, 19, 179),
-//                    child: Column(
-//                      children: <Widget>[
-//                        Container(
-//                          decoration: BoxDecoration(
-//                            borderRadius: BorderRadius.only(
-//                                topLeft: Radius.circular(20.0),
-//                                topRight: Radius.circular(20.0)),
-//                            image: DecorationImage(
-//                                image: AssetImage(
-//                                    planetCard[x].cardImage
-//                                ),
-////                                NetworkImage(),
-//                                fit: BoxFit.cover
-//                            ),
-//                          ),
-//                          height: 300.0,
-//                          width: 320.0,
-//                        ),
-//                        Container(
-//                          width: 320,
-//                          padding: EdgeInsets.only(top: 10.0, bottom: 10.0, left: 10,right: 10),
-//                          child: Column(
-//                            children: <Widget> [
-//                              Text(planetCard[x].cardTitle,
-//                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.amber),
-//                              ),
-//                              Text(planetCard[x].cardText,
-//                                style: TextStyle(fontSize: 20, color: Colors.white),
-//                              ),
-//                          ],
-//                          )
-//                        )
-//                      ],
-//                    )),
-//              )
-//          ),
+                },
+                /// 드래그하면 보여지는 카드
+                child:
+                CustomCard(planetCard[x]),
+              ),
+              child: GestureDetector(
+              onTap:(){
+                //nothing to do
+              },
+                /// 기본적으로 화면에 띄워지는 카드
+                child:
+                CustomCard(planetCard[x]),
+              )
+          ),
         ),
       );
     }
@@ -303,4 +238,81 @@ class HomePageState extends State<HomePage> {
   }
 }
 
+class CustomCard extends StatefulWidget{
+  final Widget child;
+  final PlanetCard _planetCard;
+  CustomCard(this._planetCard, {this.child});
+//  CustomCard(planetCard, {this.child});
+//    this._planetCard = planetCard.clone();
+//  };
+
+  @override
+//  State<StatefulWidget> createState(){
+//    return _CustomCardState(this._planetCard);
+//  }
+//  _CustomCardState createState(){
+//    return
+//  }
+  _CustomCardState createState() => _CustomCardState(this._planetCard);
+}
+
+class _CustomCardState extends State<CustomCard> {
+
+  PlanetCard _planetCard;
+  _CustomCardState(this._planetCard);
+
+//  PlanetCard _planetCard;
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext content){
+    return Card(
+        elevation: 8.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        color: Color.fromARGB(250, 112, 19, 179),
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0)),
+                image: DecorationImage(
+                    image: AssetImage(
+                      _planetCard.cardImage,
+                    ),
+                    fit: BoxFit.cover
+                ),
+              ),
+              height: 300.0,
+              width: 320.0,
+            ),
+            Container(
+                width: 320,
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0, left: 10,right: 10),
+                child: Column(
+                  children: <Widget> [
+                    Text(_planetCard.cardTitle,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.amber),
+                    ),
+                    Text(_planetCard.cardText,
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ],
+                )
+            )
+          ],
+        ));
+  }
+}
 
